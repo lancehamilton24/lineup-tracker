f0daae955155e13df8a07ebc30aa399fb2cf3656
Lance Hamilton <lance.hamilton24@gmail.com>
2019-06-03 12:03:01 -0500

    editing lineup name

--- src/components/LineupItem/LineupItem.js
+++ src/components/LineupItem/LineupItem.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import PropTypes from 'prop-types';
-import lineupShape from '../../../helpers/propz/lineupShape';
+import lineupShape from '../../helpers/propz/lineupShape';
 import './LineupItem.scss';
 
 class LineupItem extends React.Component {
-  // state = {
-  //   currentDate: new Date(),
-  // }
-
   static propTypes = {
     lineup: lineupShape.lineupShape,
     deleteSingleLineup: PropTypes.func,
+    passLineupToEdit: PropTypes.func,
   }
 
   deleteLineup = (e) => {
     e.preventDefault();
     const { deleteSingleLineup, lineup } = this.props;
     deleteSingleLineup(lineup.id);
+  }
+
+  editLineup = (e) => {
+    e.preventDefault();
+    const { passLineupToEdit, lineup } = this.props;
+    passLineupToEdit(lineup.id);
   }
 
   render() {
@@ -27,6 +30,7 @@ class LineupItem extends React.Component {
       <div className="lineupCards card">
         <div className="card-body">
         <span className="col-3">{lineup.name}</span>
+        <Button outline color="info" onClick={this.editLineup}>Edit</Button>
         <Button outline color="info" onClick={this.deleteLineup}>Delete</Button>
         </div>
       </div>
--- src/components/pages/Lineups/Lineups.js
+++ src/components/pages/Lineups/Lineups.js
@@ -1,6 +1,7 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import './Lineups.scss';
-import LineupItem from '../LineupItem/LineupItem';
+import LineupItem from '../../LineupItem/LineupItem';
 // import authRequests from '../../../helpers/data/authRequests';
 import LineupForm from '../LineupForm/LineupForm';
 import lineupRequests from '../../../helpers/data/lineupRequests';
@@ -9,10 +10,14 @@ import lineupShape from '../../../helpers/propz/lineupShape';
 class Lineups extends React.Component {
   state = {
     lineups: [],
+    lineupId: '',
+    isEditing: false,
+    editId: '-1',
   }
 
   static propTypes = {
     lineup: lineupShape.lineupShape,
+    // passLineupToEdit: PropTypes.func,
   }
 
   getLineups = () => {
@@ -50,15 +55,45 @@ class Lineups extends React.Component {
       .catch(err => console.error('error with lineup delete', err));
   }
 
+  formSubmitLineup = (newLineupName) => {
+    const { isEditing, editId } = this.state;
+    if (isEditing) {
+      lineupRequests.updateLineup(editId, newLineupName)
+        .then(() => {
+          lineupRequests.getAllLineups()
+            .then((lineups) => {
+              this.setState({ lineups, isEditing: false, editId: '-1' });
+            });
+        })
+        .catch(err => console.error('error with lineups post', err));
+    } else {
+      lineupRequests.postRequest(newLineupName)
+        .then(() => {
+          lineupRequests.getAllLineups()
+            .then((lineups) => {
+              this.setState({ newLineupName });
+              // this.setState({ players: [] });
+              // this.setState({ newLineupName: '' });
+            });
+        })
+        .catch(err => console.error('error with lineups post', err));
+    }
+  }
+
+  passLineupToEdit = lineupId => this.setState({ isEditing: true, editId: lineupId });
+
   render() {
     const {
       lineups,
+      isEditing,
+      editId,
     } = this.state;
 
     const lineupItems = lineups.map(lineup => (
       <LineupItem
         lineup={lineup}
         key={lineup.id}
+        passLineupToEdit={this.passLineupToEdit}
         deleteSingleLineup={this.deleteOne}
       />
     ));
@@ -67,7 +102,9 @@ class Lineups extends React.Component {
     return (
       <div className='lineups'>
         <div className="row">
-        <LineupForm onSubmit={this.formSubmitLineups} />
+        <LineupForm onSubmit={this.formSubmitLineup} 
+            isEditing={isEditing}
+            editId={editId} />
         </div>
         <div className="existingLineups">{lineupItems}</div>
       </div>
